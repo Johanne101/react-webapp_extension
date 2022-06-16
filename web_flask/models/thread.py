55a8c65e16f0cddeb7d631bb638c13af69ae0d49
WeatherBoyGPR <gp.rossijr@gmail.com>
2022-06-16 17:32:04 +0000

    added what is present of class Thread

---
+++ web_flask/models/thread.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Contains Thread class """
+
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+
+class Thread(BaseModel, Base):
+    """ Representation of individual thread """
+    url_hash = ""
+    post_count = ""
+
+    def __init__(self, *args, **kwargs)
+        """ Initializes Thread """
+        super().__init__(*args, **kwargs)
+        if kwargs.get("url", None) and type(self.url) is str:
+            self.url_hash = str(hash_func(self.url))
