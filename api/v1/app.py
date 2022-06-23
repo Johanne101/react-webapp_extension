b9fbfced9ba47d09f66ef0e4337f37e127a9a855
Restyled.io <commits@restyled.io>
2022-06-23 17:18:07 +0000

    Restyled by reorder-python-imports

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,9 @@
 """ first endpoint for status of our API """
 from os import getenv
 
-from flask import Blueprint, Flask, jsonify
+from flask import Blueprint
+from flask import Flask
+from flask import jsonify
 from flask_cors import CORS
 
 from api.v1.views import app_views
