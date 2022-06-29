226c7eb51c6e8fa5f9e2f9cf98ff7f0a9c2e4247
johanne101 <jlopez0493@gmail.com>
2022-06-29 17:20:35 +0000

    [add] fetch route

--- src/popup.jsx
+++ src/popup.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
+import fetch from 'node-fetch';
 
 function Popup() {
+
+const response = await fetch('localhost:3000');
+const body = await response.text();
+
+console.log(body);
     return (
         <div>
             <h1>Hello, world!</h1>
