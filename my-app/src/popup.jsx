55200a334615c9fcd23524f837f26ddfc99cd4ac
johanne101 <jlopez0493@gmail.com>
2022-07-01 01:45:10 +0000

    [edit] popup react app

--- my-app/src/popup.jsx
+++ my-app/src/popup.jsx
@@ -5,8 +5,7 @@ const request = require('request');
 
 function Popup() {
 
-const response = await fetch('localhost:3000');
-const body = await response.text();
+
 fetch('https://jsonplaceholder.typicode.com/todos/1')
   .then(response => response.json())
   .then(json => console.log(json))
