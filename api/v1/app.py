0c149df6f33ba0ca042a4abbbc11587a43fea5ab
johanne101 <jlopez0493@gmail.com>
2022-07-05 20:10:32 -0400

    [add] cors

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,12 +2,14 @@
 """ API Backend for user feedback application """
 
 from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
 from api.v1.views import app_views
 from models import storage
 from models.post import Post
 from models.thread import Thread
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(app_views)
 
 if __name__ == '__main__':
