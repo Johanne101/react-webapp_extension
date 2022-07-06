23d4542c925f8c12c6514ee95099e3a69f6638ca
johanne101 <jlopez0493@gmail.com>
2022-07-05 20:09:46 -0400

    [add] post resquest

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import fetch from 'node-fetch'
 import React from 'react';
-// import { Component } from 'react';
+// import React { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-  class App extends React.Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
   
@@ -30,14 +30,50 @@ import './App.css';
             "__class__": "Post"
           }
       ],
+      postsText: ""
+      // console.log(this.state.postsText)
     };
   }
-  componentDidMount() { 
-    // const response = fetch('127.0.0.1:5000/api/v1/posts');
-    // const data = response.json();
-    
-    // this.setState({ posts : data })
+  componentDidMount() {
+    this.initthread();
   }
+
+  
+  handleChange = e => {
+    this.setState({
+      // ...state,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  async initthread() {
+    let thread_id;
+    try {
+      const res = await fetch('http://127.0.0.1:5000/api/v1/threads',
+      {
+        method: 'POST',
+        headers: {"Content-Type": "application/json"}, 
+        body: JSON.stringify({url:'google.com'})
+      });
+      thread_id = await res.text();
+    } catch(e){
+      console.log(e);
+    }
+    if(!thread_id){
+      const response = await fetch('http://127.0.0.1:5000/api/v1/thread_id/localhost'); 
+      thread_id = await response.text();
+      thread_id = thread_id.slice(1,thread_id.length-2)  
+    }
+    const response_2 = await fetch(`http://127.0.0.1:5000/api/v1/threads/${thread_id}`)
+    const thread = await response_2.json();
+    const post_list = [];
+    for(const el of thread.post_list) {
+      const res = await fetch(`http://127.0.0.1:5000/api/v1/posts/${el}`)
+      post_list.push(await res.json())
+    }
+    this.setState({ posts : post_list })
+  }
+  
   create_posts() {
     return this.state.posts.map((element) =>  
       <div className="App-link">
@@ -46,21 +82,19 @@ import './App.css';
   }
   render() {
     const posts = this.create_posts();
+    console.log(this.state.postsText)
     return (
       <div className="App">
-        
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload</p>
-            {/* <div className="App-link">Post:post1</div>  
-            <div className="App-link">Post:post2</div>*/}
             {posts}
           <div><button>Like</button><button>Dislike</button></div>
           <div>
             <body>
               <form>
-                Post: <input type=""></input>
+                Post: <input name="postsText" type="text" onChange={this.handleChange} />
               </form>
             </body>
           </div>
